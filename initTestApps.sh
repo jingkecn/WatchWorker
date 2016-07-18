00a6cb90e9ad83bcfb38e475e8d108b2675658b1
jingkecn <jing.ke@wiztivi.com>
2016-07-18 11:32:56 +0200

    Reinit folder structure

--- initTestApps.sh
+++ initTestApps.sh
@@ -4,7 +4,7 @@
 # ID="com.wiztivi.examples.smartwatchworker"
 # NAME="SmartWatchWorkerExample"
 
-cordova create SmartWatchWorkerExample com.wiztivi.examples.smartwatchworker SmartWatchWorkerExample
-cd SmartWatchWorkerExample
+cordova create SmartWatchWorkerExample com.wiztivi.examples.smartwatchworker testapps/SmartWatchWorkerExample
+cd testapps/SmartWatchWorkerExample
 cordova plugin add ../../ --save
 cordova plugin add http://git-wip-us.apache.org/repos/asf/cordova-plugin-test-framework.git --save
