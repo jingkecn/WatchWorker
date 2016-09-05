267f43ef23d14be2b6ca66c5bc7921a9b3975d13
Jing KE <jingkecn@gmail.com>
2016-09-05 15:30:13 +0200

    Remove unused files

--- www/Tests/SharedWorker/SharedWorkerScope.js
+++
@@ -1,8 +0,0 @@
-onconnect = function (event) {
-    console.log("On SharedWorkerGlobalScope connected", event);
-    var port = event.ports[0];
-    port.addEventListener("message", function (event) {
-        port.postMessage(event.data);
-    });
-    port.start();
-};
--- www/Tests/Worker/WorkerScope.js
+++
@@ -1,4 +0,0 @@
-onmessage = function(event) {
-    console.log("Message from outside port", event);
-    postMessage(event.data);
-};
