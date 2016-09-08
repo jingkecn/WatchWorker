eb544f8d85a5dbdc2898176ffefda3248d3178a1
Jing KE <jingkecn@users.noreply.github.com>
2016-09-08 14:23:30 +0200

    Delete runPluginInitializer.sh

--- runPluginInitializer.sh
+++
@@ -1,7 +0,0 @@
-##!/bin/sh
-
-# NAME=WatchWorker
-# PLUGIN_ID=com-wiztivi-cordova-plugin-watchworker
-# PLUGIN_VERSION=0.0.1
-# PATH=..
-plugman create --name WatchWorker --plugin_id com-wiztivi-cordova-plugin-watchworker --plugin_version 0.0.1 --path ..
